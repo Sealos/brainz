168a1faadac7e631b8984bf3c55edda45e205a4f
Stefano De Colli <sdecolli@gmail.com>
2015-03-22 17:46:56 -0430

    Get user inforamition

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -21,6 +21,7 @@ exports.createUser = function(req, res, next) {
 	req.checkBody('email', 'email is required').notEmpty();
 	req.checkBody('email', 'email must have a valid format').optional().isEmail();
 	req.checkBody('dateOfBirth', 'dateOfBirth is required, YYYY/MM/DD').notEmpty();
+	req.checkBody('password', 'password is required').notEmpty();
 
 	var errors = req.validationErrors();
 	if (errors)
@@ -159,5 +160,16 @@ exports.getUserLog = function(req, res, next) {
 		return res.status(200).json({
 			log: log.log
 		});
+	});
+};
+
+exports.getUser = function(req, res, next) {
+	req.user.apiVersion = undefined;
+	req.user.lastKnownLoc = undefined;
+	req.user.permission = undefined;
+	req.user.lastAccess = undefined;
+
+	return res.status(200).json({
+		user: req.user
 	});
 };
